0e86590fb9e3f95d4b3cda357e5aad420b8ccf98
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2012-04-04 08:38:58 +0300

    Add view that generates a HTML5 video player using flavors available through Kaltura API.

---
+++ views/default/kaltura/player.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * View HTML5 video player
+ * 
+ * Get all available video flavores through Kaltura API.
+ * 
+ * @uses $vars['entity] KalturaVideo object
+ */
+
+$entity = elgg_extract('entity', $vars);
+$entry_id = $entity->getEntryId();
+ 
+$kmodel = KalturaModel::getInstance();
+$kmodel->startSession();
+
+$flavorAsset = $kmodel->client->flavorAsset;
+$results = $flavorAsset->getFlavorAssetsWithParams($entry_id);
+//$results = $flavorAsset->getWebPlayableByEntryId($entry_id);
+
+$plugin = elgg_get_plugin_from_id('kaltura_video');
+$server_url = $plugin->kaltura_server_url;
+$p = $plugin->partner_id;
+$thumb = $plugin->kaltura_video_thumbnail;
+?>
+<video width="730" height="410" controls="true" poster="" class="video-js vjs-default-skin" id="video-js-player" data-setup="{}">
+
+<?php
+// Here we have all the available flavors
+$formats = array();
+$urls = array();
+foreach($results as $result) {
+	// Flavor Params can exist without Flavor Asset & vice versa. Skip if no flavor.
+	if (empty($result->flavorAsset->id) ||
+		empty($result->flavorParams->format)) {
+		continue;
+	}
+	
+	//$result->flavorAsset->height;
+	//$result->flavorAsset->width;
+	
+	$format = $result->flavorParams->format;
+	// We do not want to support flash
+	if ($format === 'flv') {
+		continue;
+	}
+	$ext = $result->flavorAsset->fileExt;	
+	$id  = $result->flavorAsset->id;
+	$entryId = $result->entryId;
+
+	$url = "{$server_url}p/{$p}/sp/{$p}00/serveFlavor/flavorId/{$id}/name/{$id}.{$ext}";
+	$video_source = "<source src=\"$url\" type=\"video/{$format}\" />";
+	echo $video_source;
+
+	// Save flavor url so it can be used in flavor selector
+	$urls[$url] = $result->flavorParams->name;
+}
+?>
+</video>
+
+<?php
+
+echo elgg_view('input/dropdown', array(
+	'name' => 'video-selector',
+	'id' => 'video-selector',
+	'options_values' => $urls
+));
